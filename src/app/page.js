8a3a152f595adcb11797248a8704da73e0b8e620
Psycho <psychogas69@gmail.com>
2024-06-07 21:59:05 +0530

    change bg color back

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,13 @@ export default function Page() {
   const [token, setToken] = useState("");
 
   return (
-    <div style={{ backgroundColor: "#181716" }}>
+    <>
       <h1 className="text-5xl md:text-6xl font-extrabold text-center text-white drop-shadow-lg">
         SpotiFire
       </h1>
       {!token && <SignIn token={token} setToken={setToken} />}
       <SignOut token={token} setToken={setToken} />
       {token && <Tracks token={token} />}
-    </div>
+    </>
   );
 }
