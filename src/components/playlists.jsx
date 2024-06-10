f265d34678e9a5d6bebe8bed13d532da4d53daeb
Psycho <psychogas69@gmail.com>
2024-06-10 16:12:47 +0530

    made the similar thing for playlists but the track number isnt working, gotta fix

--- src/components/playlists.jsx
+++ src/components/playlists.jsx
@@ -1,9 +1,31 @@
+import Card from "./ui/card";
 export default function Playlists({ userPlaylists }) {
   return (
     <>
-      <ul>
+      <ul
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          paddingLeft: "100px",
+          marginBottom: "0",
+        }}
+      >
         {userPlaylists.items.map((playlist, index) => (
-          <li key={playlist.id}>Playlist Name - {playlist.name}</li>
+          <li
+            key={playlist.id}
+            style={{ flex: "0 0 calc(25% - 10px)", marginBottom: "10px" }}
+            className="py-5"
+          >
+            <div className="py-10px px-10px">
+              <Card
+                Name={playlist.name}
+                Cover={playlist.images[0].url}
+                Number={playlist.tracks.total}
+              />
+              {console.log(playlist.tracks.total)}
+            </div>
+          </li>
         ))}
       </ul>
     </>
