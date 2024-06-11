2f30b6421b8dcac2b88b07cd1f905bd5b3fe1313
Psycho <psychogas69@gmail.com>
2024-06-12 01:18:11 +0530

    tested

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -5,10 +5,10 @@ import { useEffect } from "react";
 
 export default function SignIn({ token, setToken }) {
   const clientId = "49e3ee9fe2494ec5a7908fba1799d7bf";
-  const redirectUri = "https://spotifire-psi.vercel.app";
+  const redirectUri = "http://localhost:3000";
   const authEndpoint = "https://accounts.spotify.com/authorize";
   const responseType = "token";
-  const scopes = "playlist-read-private";
+  const scopes = "user-library-read+playlist-read-private";
 
   useEffect(() => {
     const hash = window.location.hash;
